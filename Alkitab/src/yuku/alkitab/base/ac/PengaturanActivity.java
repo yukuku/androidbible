349a1ad81a524bb7eb7b69b64975e95c03ca1092
Yuku on yuku5 <yukuku@gmail.com>
2012-06-05 16:17:56 +0700

    Use the summary field on preference to show the current value instead of the mostly useless description.

--- Alkitab/src/yuku/alkitab/base/ac/PengaturanActivity.java
+++ Alkitab/src/yuku/alkitab/base/ac/PengaturanActivity.java
@@ -1,18 +1,53 @@
 package yuku.alkitab.base.ac;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.view.MenuItem;
 
 import yuku.alkitab.R;
 import yuku.alkitab.base.ac.base.BaseActivity;
+import yuku.alkitab.base.storage.Preferences;
 
 public class PengaturanActivity extends PreferenceActivity {
+	interface PreferenceUpdate {
+		String withValue(Object value);
+	}
+	
 	@SuppressWarnings("deprecation") @Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		addPreferencesFromResource(R.xml.pengaturan);
 		setTitle(R.string.pengaturan_alkitab);
+		
+		autoUpdateSummary(R.string.pref_ukuranHuruf2_key, new PreferenceUpdate() {
+			@Override public String withValue(Object value) {
+				if (value == null) return "Default";
+				float dp = (Float) value;
+				return String.format("%.1f dp (%.1f pt)", dp, dp * 0.45f);
+			}
+		});
+		
+		autoUpdateSummary(R.string.pref_jenisHuruf_key, new PreferenceUpdate() {
+			@Override public String withValue(Object value) {
+				return value != null? (String) value: "Default";
+			}
+		});
+	}
+
+	private void autoUpdateSummary(int keyResId, final PreferenceUpdate pu) {
+		String key = getString(keyResId);
+		@SuppressWarnings("deprecation") final Preference pref = findPreference(key);
+		if (pref != null) {
+			pref.setSummary(pu.withValue(Preferences.get(key)));
+			pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+				@Override public boolean onPreferenceChange(Preference preference, Object newValue) {
+					pref.setSummary(pu.withValue(newValue));
+					return true;
+				}
+			});
+		}
 	}
 	
 	@Override public boolean onOptionsItemSelected(MenuItem item) {
