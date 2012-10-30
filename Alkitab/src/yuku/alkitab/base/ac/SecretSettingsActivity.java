f5960c4f923b5b2ea61dbcddd1f32bc8df24c257
Yuku on yuku5 <yukuku@gmail.com>
2012-10-30 13:36:32 +0700

    First secret setting: Use legacy verse renderer

--- Alkitab/src/yuku/alkitab/base/ac/SecretSettingsActivity.java
+++ Alkitab/src/yuku/alkitab/base/ac/SecretSettingsActivity.java
@@ -2,14 +2,15 @@ package yuku.alkitab.base.ac;
 
 import android.os.Bundle;
 
+import yuku.alkitab.R;
 import yuku.alkitab.base.ac.base.BasePreferenceActivity;
 
 public class SecretSettingsActivity extends BasePreferenceActivity {
 	public static final String TAG = SecretSettingsActivity.class.getSimpleName();
 	
-	@Override protected void onCreate(Bundle savedInstanceState) {
+	@SuppressWarnings("deprecation") @Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		// addPreferencesFromResource(R.xml.secret_settings);
+		addPreferencesFromResource(R.xml.secret_settings);
 	}
 }
