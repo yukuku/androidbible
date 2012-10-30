274d61dccfcb14219661a6dbc005c20db4de1f57
Yuku on yuku5 <yukuku@gmail.com>
2012-10-30 13:29:53 +0700

    Create secret settings activity

---
+++ Alkitab/src/yuku/alkitab/base/ac/SecretSettingsActivity.java
@@ -0,0 +1,15 @@
+package yuku.alkitab.base.ac;
+
+import android.os.Bundle;
+
+import yuku.alkitab.base.ac.base.BasePreferenceActivity;
+
+public class SecretSettingsActivity extends BasePreferenceActivity {
+	public static final String TAG = SecretSettingsActivity.class.getSimpleName();
+	
+	@Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// addPreferencesFromResource(R.xml.secret_settings);
+	}
+}
