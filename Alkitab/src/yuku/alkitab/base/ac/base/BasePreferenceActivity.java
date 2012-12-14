97ad0533e14a76c73452564e2df30f645d37f743
Yuku on yuku5 <yukuku@gmail.com>
2012-12-14 14:26:03 +0700

    Google analytics v2 integrated

--- Alkitab/src/yuku/alkitab/base/ac/base/BasePreferenceActivity.java
+++ Alkitab/src/yuku/alkitab/base/ac/base/BasePreferenceActivity.java
@@ -1,6 +1,16 @@
 package yuku.alkitab.base.ac.base;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.google.analytics.tracking.android.EasyTracker;
 
 public abstract class BasePreferenceActivity extends SherlockPreferenceActivity {
+	@Override protected void onStart() {
+		super.onStart();
+	    EasyTracker.getInstance().activityStart(this); 
+	}
+	
+	@Override protected void onStop() {
+		super.onStop();
+		EasyTracker.getInstance().activityStop(this);
+	}
 }
