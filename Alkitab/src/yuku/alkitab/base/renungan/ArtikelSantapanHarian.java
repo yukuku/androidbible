d63451d3fe9b46cbd903f2e1889c46ea155cb763
Yuku on yuku5 <yukuku@gmail.com>
2012-12-21 19:04:08 +0700

    Devotion copyright text made smaller

--- Alkitab/src/yuku/alkitab/base/renungan/ArtikelSantapanHarian.java
+++ Alkitab/src/yuku/alkitab/base/renungan/ArtikelSantapanHarian.java
@@ -12,10 +12,10 @@ public class ArtikelSantapanHarian extends ArtikelDariSabda {
 
 	@Override public CharSequence getKopiraitHtml() {
 		return "__________<br/>" +
-		"Santapan Harian / e-Santapan Harian<br/>" + 
+		"<small>Santapan Harian / e-Santapan Harian<br/>" + 
 		"Bahan saat teduh yang diterbitkan secara teratur oleh Persekutuan Pembaca Alkitab (PPA) dan diterbitkan secara elektronik oleh Yayasan Lembaga SABDA (YLSA). <br/>" + 
 		"© 1999-2012 Yayasan Lembaga SABDA (YLSA). <br/>" + 
-		"Isi boleh disimpan untuk tujuan pribadi atau non-komersial. Atas setiap publikasi atau pencetakan wajib menyebutkan alamat situs SABDA.org sebagai sumber tulisan dan mengirim pemberitahuan ke webmaster@sabda.org";
+		"Isi boleh disimpan untuk tujuan pribadi atau non-komersial. Atas setiap publikasi atau pencetakan wajib menyebutkan alamat situs SABDA.org sebagai sumber tulisan dan mengirim pemberitahuan ke webmaster@sabda.org</small>";
 	}
 
 	@Override
