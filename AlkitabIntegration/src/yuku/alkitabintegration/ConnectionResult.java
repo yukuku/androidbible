09982e30822b2b85b267edcd99548ee046fa70c8
Yuku on yuku5 <yukuku@gmail.com>
2013-03-11 17:25:49 +0800

    Add AlkitabIntegration library project, for use by other apps to easily integrate Alkitab app data and activities.

---
+++ AlkitabIntegration/src/yuku/alkitabintegration/ConnectionResult.java
@@ -0,0 +1,20 @@
+package yuku.alkitabintegration;
+
+public class ConnectionResult {
+	public static final String TAG = ConnectionResult.class.getSimpleName();
+	
+	/** The connection was successful. */
+	public static final int SUCCESS = 0;
+
+	/** Alkitab app is missing on this device. */
+	public static final int APP_MISSING = 1;
+	
+	/** The installed version of Alkitab app is out of date. */ 
+	public static final int APP_VERSION_UPDATE_REQUIRED = 2;
+	
+	/** The provider component of Alkitab has been disabled on this device. */ 
+	public static final int PROVIDER_DISABLED = 3;
+
+	/** An internal error occurred. Retrying should resolve the problem. */
+	public static final int INTERNAL_ERROR = 8;
+}
