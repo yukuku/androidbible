f1d92d4aaded143b5b19dbab1a49f523dd604d9d
Yuku on yuku5 <yukuku@gmail.com>
2013-03-13 18:35:46 +0800

    Optimized parameter passing from TextSectionReader to SnappyInputStream

--- Alkitab/src/yuku/alkitab/yes2/Yes2Reader.java
+++ Alkitab/src/yuku/alkitab/yes2/Yes2Reader.java
@@ -55,28 +55,21 @@ public class Yes2Reader implements BibleReader {
 	
 	static class SnappyInputStream extends InputStream {
 		private final Snappy snappy;
-		private RandomInputStream file;
-		private long baseOffset;
-		private int block_size;
+		private final RandomInputStream file;
+		private final long baseOffset;
+		private final int[] compressed_block_sizes;
+		private final int[] compressed_block_offsets;
 		private int current_block_index;
 		private int current_block_skip;
-		private int[] compressed_block_sizes;
-		private int[] compressed_block_offsets;
 		private byte[] compressed_buf;
 		private int uncompressed_block_index = -1;
 		private byte[] uncompressed_buf;
 		private int uncompressed_len;
 		
-		public SnappyInputStream() {
+		public SnappyInputStream(RandomInputStream file, long baseOffset, int block_size, int[] compressed_block_sizes, int[] compressed_block_offsets) {
 			this.snappy = new Snappy.Factory().newInstance();
-		}
-
-		void init(RandomInputStream file, long baseOffset, int block_size, int current_block_index, int current_block_skip, int[] compressed_block_sizes, int[] compressed_block_offsets) throws IOException {
 			this.file = file;
 			this.baseOffset = baseOffset;
-			this.block_size = block_size;
-			this.current_block_index = current_block_index;
-			this.current_block_skip = current_block_skip;
 			this.compressed_block_sizes = compressed_block_sizes;
 			this.compressed_block_offsets = compressed_block_offsets;
 			if (compressed_buf == null || compressed_buf.length < snappy.maxCompressedLength(block_size)) {
@@ -85,10 +78,15 @@ public class Yes2Reader implements BibleReader {
 			if (uncompressed_buf == null || uncompressed_buf.length != block_size) {
 				uncompressed_buf = new byte[block_size];
 			}
+		}
+
+		public void prepareForRead(int current_block_index, int current_block_skip) throws IOException {
+			this.current_block_index = current_block_index;
+			this.current_block_skip = current_block_skip;
 			prepareBuffer();
 		}
 		
-		void prepareBuffer() throws IOException {
+		private void prepareBuffer() throws IOException {
 			int block_index = current_block_index;
 			
 			// if uncompressed_block_index is already equal to the requested block_index
@@ -158,6 +156,7 @@ public class Yes2Reader implements BibleReader {
 	 * and also understands the text section attributes (compression, encryption etc.)
 	 */
 	static class TextSectionReader {
+		private RandomInputStream file_;
 		private final Yes2VerseTextDecoder decoder_;
 		private final long sectionContentOffset_;
 		
@@ -166,7 +165,8 @@ public class Yes2Reader implements BibleReader {
 		private int[] compressed_block_sizes;
 		private int[] compressed_offsets;
 		
-		public TextSectionReader(Yes2VerseTextDecoder decoder, ValueMap sectionAttributes, long sectionContentOffset) throws Exception {
+		public TextSectionReader(RandomInputStream file, Yes2VerseTextDecoder decoder, ValueMap sectionAttributes, long sectionContentOffset) throws Exception {
+			file_ = file;
 			decoder_ = decoder;
 			sectionContentOffset_ = sectionContentOffset;
 			
@@ -189,14 +189,14 @@ public class Yes2Reader implements BibleReader {
 						}
 						compressed_offsets[compressed_block_sizes.length] = c;
 					}
-					snappyInputStream = new SnappyInputStream();
+					snappyInputStream = new SnappyInputStream(file_, sectionContentOffset, block_size, compressed_block_sizes, compressed_offsets);
 				} else {
 					throw new Exception("Compression " + compressionName + " is not supported");
 				}
 			}
 		}
 
-		public Yes2SingleChapterVerses loadVerseText(Yes2Book yes2Book, int chapter_1, boolean dontSeparateVerses, boolean lowercase, RandomInputStream file_) throws Exception {
+		public Yes2SingleChapterVerses loadVerseText(Yes2Book yes2Book, int chapter_1, boolean dontSeparateVerses, boolean lowercase) throws Exception {
 			int contentOffset = yes2Book.offset; 
 			contentOffset += yes2Book.chapter_offsets[chapter_1 - 1];
 			
@@ -211,7 +211,7 @@ public class Yes2Reader implements BibleReader {
 					Log.d(TAG, "skipping " + block_skip + " uncompressed bytes");
 				}
 				
-				snappyInputStream.init(file_, sectionContentOffset_, block_size, block_index, block_skip, compressed_block_sizes, compressed_offsets);
+				snappyInputStream.prepareForRead(block_index, block_skip);
 				br = new BintexReader(snappyInputStream);
 			} else {
 				file_.seek(sectionContentOffset_ + contentOffset);
@@ -347,10 +347,10 @@ public class Yes2Reader implements BibleReader {
 					decoder = new Yes2VerseTextDecoder.Ascii();
 				}
 				
-				textSectionReader_ = new TextSectionReader(decoder, sectionAttributes, sectionContentOffset);
+				textSectionReader_ = new TextSectionReader(file_, decoder, sectionAttributes, sectionContentOffset);
 			}
 			
-			return textSectionReader_.loadVerseText(yes2Book, chapter_1, dontSeparateVerses, lowercase, file_);
+			return textSectionReader_.loadVerseText(yes2Book, chapter_1, dontSeparateVerses, lowercase);
 		} catch (Exception e) {
 			Log.e(TAG, "loadVerseText error", e); //$NON-NLS-1$
 			return null;
