05aa99d22057e9aa51a801d2438b50d557770f04
Rudy <inirudy@gmail.com>
2013-10-02 18:56:01 +0800

    Add bootcomplete receiver.

---
+++ AndroidBibleReminder/src/yuku/alkitab/reminder/br/BootCompleteReceiver.java
@@ -0,0 +1,11 @@
+package yuku.alkitab.reminder.br;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+	public static final String TAG = BootCompleteReceiver.class.getSimpleName();
+	public void onReceive(Context context, Intent intent) {
+	}
+}
