be78cb95a14f2429182a36d03025bdf6ed0c77e5
Yuku on yuku5 <yukuku@gmail.com>
2013-11-11 16:12:29 +0800

    Fix because of rebase

--- Alkitab/src/yuku/alkitab/base/ac/HelpActivity.java
+++ Alkitab/src/yuku/alkitab/base/ac/HelpActivity.java
@@ -23,9 +23,9 @@ import yuku.afw.V;
 import yuku.alkitab.base.U;
 import yuku.alkitab.base.ac.base.BaseActivity;
 import yuku.alkitab.base.dialog.VersesDialog;
-import yuku.alkitab.base.util.IntArrayList;
 import yuku.alkitab.base.util.TargetDecoder;
 import yuku.alkitab.debug.R;
+import yuku.alkitab.util.IntArrayList;
 import yuku.alkitabintegration.display.Launcher;
 
 public class HelpActivity extends BaseActivity {
