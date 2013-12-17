8f5be6ca7a8de39a8bf1ece3e8d84ef1e5b4ec9c
Yuku on yuku5 <yukuku@gmail.com>
2013-12-17 15:17:49 +0800

    trivial

--- AlkitabConverter/src/yuku/alkitabconverter/util/UsfmBookName.java
+++ AlkitabConverter/src/yuku/alkitabconverter/util/UsfmBookName.java
@@ -5,7 +5,7 @@ public class UsfmBookName {
 
 	static String[] ntBookNames = "MAT MRK LUK JHN ACT ROM 1CO 2CO GAL EPH PHP COL 1TH 2TH 1TI 2TI TIT PHM HEB JAS 1PE 2PE 1JN 2JN 3JN JUD REV".split(" ");
 	
-	public static int toKitab0(String name) {
+	public static int toBookId(String name) {
 		for (int i = 0; i < ntBookNames.length; i++) {
 			if (name.equals(ntBookNames[i])) {
 				return 39 + i;
