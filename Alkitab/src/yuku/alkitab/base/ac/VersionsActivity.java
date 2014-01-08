35fe5a7cdfe8ca75b36670a576c77bde499b054c
Yuku on yuku5 <yukuku@gmail.com>
2014-01-08 18:41:57 +0800

    Revert "Add ml-bsi version"
    This reverts commit 038d64637554ed3d7a693b1c01d7331a92ff99d0.

--- Alkitab/src/yuku/alkitab/base/ac/VersionsActivity.java
+++ Alkitab/src/yuku/alkitab/base/ac/VersionsActivity.java
@@ -455,7 +455,7 @@ public class VersionsActivity extends BaseActivity {
 										Toast.LENGTH_LONG).show();
 
 									final String locale = mv.locale;
-									if ("ta".equals(locale) || "te".equals(locale) || "my".equals(locale) || "el".equals(locale) || "ml".equals(locale)) {
+									if ("ta".equals(locale) || "te".equals(locale) || "my".equals(locale) || "el".equals(locale)) {
 										new AlertDialog.Builder(VersionsActivity.this)
 										.setMessage(R.string.version_download_need_fonts)
 										.setPositiveButton(R.string.ok, null)
