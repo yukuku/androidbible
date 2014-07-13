0b2a4aa84e4df399a782637e412cd53cd7f9bf2e
Yuku on yuku8 <yukuku@gmail.com>
2014-07-14 01:26:50 +0800

    add secret to check version table

--- Alkitab/src/yuku/alkitab/base/ac/SecretSettingsActivity.java
+++ Alkitab/src/yuku/alkitab/base/ac/SecretSettingsActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.Preference;
+import yuku.alkitab.base.model.MVersionDb;
 import yuku.alkitab.debug.R;
 import yuku.alkitab.base.S;
 import yuku.alkitab.base.ac.base.BasePreferenceActivity;
@@ -11,6 +12,7 @@ import yuku.alkitab.model.ProgressMark;
 import yuku.alkitab.model.ProgressMarkHistory;
 import yuku.alkitab.base.util.Sqlitil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecretSettingsActivity extends BasePreferenceActivity {
@@ -48,11 +50,32 @@ public class SecretSettingsActivity extends BasePreferenceActivity {
 		}
 	};
 
+	private Preference.OnPreferenceClickListener secret_version_table_click = new Preference.OnPreferenceClickListener() {
+		@Override
+		public boolean onPreferenceClick(final Preference preference) {
+			List<String> items = new ArrayList<>();
+
+			for (final MVersionDb mv : S.getDb().listAllVersions()) {
+				items.add(
+					String.format("filename=%s preset_name=%s modifyTime=%s active=%s locale=%s shortName=%s longName=%s description=%s",
+						mv.filename, mv.preset_name, mv.modifyTime, mv.getActive(), mv.locale, mv.shortName, mv.longName, mv.description)
+				);
+			}
+
+			new AlertDialog.Builder(SecretSettingsActivity.this)
+				.setItems(items.toArray(new String[items.size()]), null)
+				.show();
+
+			return true;
+		}
+	};
+
 	@SuppressWarnings("deprecation") @Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		addPreferencesFromResource(R.xml.secret_settings);
 
 		findPreference("secret_progress_mark_history").setOnPreferenceClickListener(secret_progress_mark_history_click);
+		findPreference("secret_version_table").setOnPreferenceClickListener(secret_version_table_click);
 	}
 }
