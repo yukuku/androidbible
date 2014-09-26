e9fd18bec3fbad3ac072b7f29314362ea99d43be
Yuku on yuku8 <yukuku@gmail.com>
2014-09-26 18:58:15 +0800

    When viewing notes, user can click on verse references to immediately open the verses in a dialog.

--- Alkitab/src/main/java/yuku/alkitab/base/ac/NoteActivity.java
+++ Alkitab/src/main/java/yuku/alkitab/base/ac/NoteActivity.java
@@ -4,15 +4,25 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.method.ArrowKeyMovementMethod;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import yuku.afw.V;
 import yuku.alkitab.base.App;
 import yuku.alkitab.base.S;
 import yuku.alkitab.base.ac.base.BaseActivity;
+import yuku.alkitab.base.dialog.VersesDialog;
+import yuku.alkitab.base.widget.CallbackSpan;
 import yuku.alkitab.debug.R;
 import yuku.alkitab.model.Marker;
+import yuku.alkitab.util.IntArrayList;
+import yuku.alkitabconverter.util.DesktopVerseFinder;
+import yuku.alkitabconverter.util.DesktopVerseParser;
+import yuku.alkitabintegration.display.Launcher;
 
 import java.util.Date;
 
@@ -59,6 +69,8 @@ public class NoteActivity extends BaseActivity {
 	int ariForNewNote;
 	int verseCountForNewNote;
 
+	boolean editingMode;
+
 	EditText tCaption;
 
 	@Override
@@ -85,7 +97,82 @@ public class NoteActivity extends BaseActivity {
 
 		if (marker != null) {
 			tCaption.setText(marker.caption);
+			// editing existing note. Make the note read-only at first.
+			setEditingMode(false);
+		} else {
+			// a new note
+			setEditingMode(true);
 		}
+	}
+
+	static class VerseSpan extends CallbackSpan<String> {
+		VerseSpan(final String verse, final OnClickListener<String> verseClickListener) {
+			super(verse, verseClickListener);
+		}
+	}
+
+	final CallbackSpan.OnClickListener<String> verseClickListener = new CallbackSpan.OnClickListener<String>() {
+		@Override
+		public void onClick(final View widget, final String verse) {
+			final IntArrayList verseRanges = DesktopVerseParser.verseStringToAri(verse);
+			if (verseRanges == null || verseRanges.size() == 0) {
+				new AlertDialog.Builder(widget.getContext())
+					.setMessage(R.string.note_activity_cannot_parse_verse)
+					.setPositiveButton(R.string.ok, null)
+					.show();
+				return;
+			}
+
+			final VersesDialog versesDialog = VersesDialog.newInstance(verseRanges);
+			versesDialog.setListener(new VersesDialog.VersesDialogListener() {
+				@Override
+				public void onVerseSelected(final VersesDialog dialog, final int ari) {
+					startActivity(Launcher.openAppAtBibleLocationWithVerseSelected(ari));
+					versesDialog.dismiss();
+				}
+			});
+			versesDialog.show(getSupportFragmentManager(), "dialog_verses");
+		}
+	};
+
+	void setEditingMode(final boolean editingMode) {
+		if (editingMode) {
+			tCaption.setMovementMethod(ArrowKeyMovementMethod.getInstance());
+			tCaption.setFocusable(true);
+			tCaption.setFocusableInTouchMode(true);
+			tCaption.setClickable(true);
+
+			// remove all verse links
+			final SpannableStringBuilder text = new SpannableStringBuilder(tCaption.getText());
+			final VerseSpan[] spans = text.getSpans(0, text.length(), VerseSpan.class);
+			if (spans != null) for (final VerseSpan span : spans) {
+				text.removeSpan(span);
+			}
+			tCaption.setText(text);
+		} else {
+			tCaption.setMovementMethod(LinkMovementMethod.getInstance());
+			tCaption.setFocusable(false);
+			tCaption.setFocusableInTouchMode(false);
+			tCaption.setClickable(true);
+
+			// put verse links
+			final SpannableStringBuilder text = new SpannableStringBuilder(tCaption.getText());
+			DesktopVerseFinder.findInText(text, new DesktopVerseFinder.DetectorListener() {
+				@Override
+				public boolean onVerseDetected(final int start, final int end, final String verse) {
+					text.setSpan(new VerseSpan(verse, verseClickListener), start, end, 0);
+					return true;
+				}
+
+				@Override
+				public void onNoMoreDetected() {}
+			});
+			tCaption.setText(text);
+		}
+
+		this.editingMode = editingMode;
+
+		invalidateOptionsMenu();
 	}
 
 	@Override
@@ -95,8 +182,25 @@ public class NoteActivity extends BaseActivity {
 	}
 
 	@Override
+	public boolean onPrepareOptionsMenu(final Menu menu) {
+		final MenuItem menuEdit = menu.findItem(R.id.menuEdit);
+		final MenuItem menuDelete = menu.findItem(R.id.menuDelete);
+		final MenuItem menuOk = menu.findItem(R.id.menuOk);
+
+		menuEdit.setVisible(!editingMode);
+		menuDelete.setVisible(editingMode);
+		menuOk.setVisible(editingMode);
+
+		return true;
+	}
+
+	@Override
 	public boolean onOptionsItemSelected(final MenuItem item) {
 		switch (item.getItemId()) {
+			case R.id.menuEdit: {
+				setEditingMode(true);
+			}
+			return true;
 			case R.id.menuDelete: {
 				// if it's indeed not exist, check if we have some text, if we do, prompt first
 				if (marker != null || tCaption.length() > 0) {
