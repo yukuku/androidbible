a017696b0a9621649d348b7942bb9ba7c831147e
Yuku on yuku8 <yukuku@gmail.com>
2014-09-30 19:39:28 +0800

    Marker_Label model

---
+++ AlkitabModel/src/main/java/yuku/alkitab/model/Marker_Label.java
@@ -0,0 +1,13 @@
+package yuku.alkitab.model;
+
+public class Marker_Label {
+	public String gid;
+	public String marker_gid;
+	public String label_gid;
+
+	private Marker_Label() {}
+
+	public static Marker_Label createEmptyMarker_Label() {
+		return new Marker_Label();
+	}
+}
