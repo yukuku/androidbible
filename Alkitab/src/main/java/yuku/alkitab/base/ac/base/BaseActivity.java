7084e410d80d14669be28ad075984e40d34a1e7e
jaya hendrick <jasat90@gmail.com>
2014-12-10 14:25:16 +0800

    set color

--- Alkitab/src/main/java/yuku/alkitab/base/ac/base/BaseActivity.java
+++ Alkitab/src/main/java/yuku/alkitab/base/ac/base/BaseActivity.java
@@ -1,14 +1,26 @@
 package yuku.alkitab.base.ac.base;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBarActivity;
 
+import yuku.alkitab.debug.R;
+
 public abstract class BaseActivity extends ActionBarActivity {
 	public static final String TAG = BaseActivity.class.getSimpleName();
 
-	protected void navigateUp() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.primary_dark));
+        }
+    }
+
+    protected void navigateUp() {
 		final Intent upIntent = NavUtils.getParentActivityIntent(this);
 		if (upIntent == null) { // not defined in manifest, let us finish() instead.
 			finish();
