01d6869fd8918a78d9efc51608b71df551fb481b
Yuku on yuku8 <yukuku@gmail.com>
2014-12-11 12:31:37 +0800

    Up button on sync login activity

--- Alkitab/src/main/java/yuku/alkitab/base/ac/base/BaseActivity.java
+++ Alkitab/src/main/java/yuku/alkitab/base/ac/base/BaseActivity.java
@@ -5,20 +5,43 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-
+import android.view.MenuItem;
 import yuku.alkitab.debug.R;
 
 public abstract class BaseActivity extends ActionBarActivity {
 	public static final String TAG = BaseActivity.class.getSimpleName();
 
-    @Override
+	private boolean withNonToolbarUpButton;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.primary_dark));
         }
     }
+
+	protected void onCreateWithNonToolbarUpButton(Bundle savedInstanceState) {
+		this.withNonToolbarUpButton = true;
+
+		super.onCreate(savedInstanceState);
+
+		final ActionBar actionBar = getSupportActionBar();
+		if (actionBar != null) {
+			actionBar.setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override public boolean onOptionsItemSelected(MenuItem item) {
+		if (withNonToolbarUpButton && item.getItemId() == android.R.id.home) {
+			navigateUp();
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 
     protected void navigateUp() {
 		final Intent upIntent = NavUtils.getParentActivityIntent(this);
