0423145669fa7ceac23cbe4f791dddda39e8571b
Yuku on yuku8 <yukuku@gmail.com>
2015-08-26 12:33:47 +0800

    Example extension: Remove unneeded code

--- extensions/example-imagesharer/app/src/main/java/yuku/alkitab.imagesharer/ShareVerseActivity.java
+++ extensions/example-imagesharer/app/src/main/java/yuku/alkitab.imagesharer/ShareVerseActivity.java
@@ -9,8 +9,6 @@ import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -71,25 +69,4 @@ public class ShareVerseActivity extends AppCompatActivity {
 		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_share_verse, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 }
