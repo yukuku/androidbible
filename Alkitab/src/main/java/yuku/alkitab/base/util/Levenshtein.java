ceabc3257dd1f6d59910f2a2af0d13f67f3ac95d
Yuku on yuku8 <yukuku@gmail.com>
2016-08-31 03:25:32 +0800

    Try to fix weird bug in Levenshtein

--- Alkitab/src/main/java/yuku/alkitab/base/util/Levenshtein.java
+++ Alkitab/src/main/java/yuku/alkitab/base/util/Levenshtein.java
@@ -7,12 +7,16 @@ public class Levenshtein {
 
 	public static int distance(String s, String t) {
 		// d is a table with m+1 rows and n+1 columns
-		int m = s.length();
-		int n = t.length();
+		final int m = s.length();
+		final int n = t.length();
 
-		int[][] d = new int[m + 1][n + 1];
+		final int[][] d = new int[m + 1][n + 1];
 
 		for (int i = 0; i <= m; i++) {
+			// workaround for seemingly bad VM: http://stackoverflow.com/questions/37193263
+			if (d[i] == null) {
+				d[i] = new int[n + 1];
+			}
 			d[i][0] = i * deletion; // deletion
 		}
 
@@ -20,15 +24,15 @@ public class Levenshtein {
 			d[0][j] = j * insertion; // insertion
 		}
 
-		for (int j = 1; j <= n; j++) { // j adalah index t
-			for (int i = 1; i <= m; i++) { // i adalah index s
+		for (int j = 1; j <= n; j++) { // j is the index to t
+			for (int i = 1; i <= m; i++) { // i is the index to s
 				if (s.charAt(i - 1) == t.charAt(j - 1)) {
-					d[i][j] = d[i - 1][j - 1] + j + j; // makin jauh samanya, makin ga penting
+					d[i][j] = d[i - 1][j - 1] + j + j; // the longer the offset difference between the same character, the less relevant it is
 				} else {
 					d[i][j] = minimum(
-							d[i - 1][j] + deletion, // deletion
-							d[i][j - 1] + insertion + (j - i), // insertion
-							d[i - 1][j - 1] + substitution // substitution
+						d[i - 1][j] + deletion, // deletion
+						d[i][j - 1] + insertion + (j - i), // insertion
+						d[i - 1][j - 1] + substitution // substitution
 					);
 				}
 			}
