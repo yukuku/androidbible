ec9139df5cc687bcdcea67b7d95b9b59c0f8bacf
Yuku on yuku8 <yukuku@gmail.com>
2017-09-15 00:51:59 +0800

    Make sure nobackuppreferences is not crashing

--- Alkitab/src/main/java/yuku/alkitab/base/storage/NoBackupSharedPreferences.java
+++ Alkitab/src/main/java/yuku/alkitab/base/storage/NoBackupSharedPreferences.java
@@ -15,13 +15,12 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class NoBackupSharedPreferences {
-	static HashMap<String, NoBackupSharedPreferences> instances = new HashMap<>();
-
-	final File file;
+	final static HashMap<String, NoBackupSharedPreferences> instances = new HashMap<>();
 
 	static class Map extends LinkedHashMap<String, Object> {}
 
-	Map map;
+	final File file;
+	final Map map;
 
 	public static NoBackupSharedPreferences get() {
 		return get("default.xml");
@@ -42,7 +41,8 @@ public class NoBackupSharedPreferences {
 			final AtomicFile atom = new AtomicFile(file);
 			try {
 				final FileInputStream fis = atom.openRead();
-				map = App.getDefaultGson().fromJson(new InputStreamReader(fis, Charset.forName("utf-8")), Map.class);
+				final Map savedMap = App.getDefaultGson().fromJson(new InputStreamReader(fis, Charset.forName("utf-8")), Map.class);
+				map = savedMap == null ? new Map() : savedMap;
 				fis.close();
 			} catch (IOException e) {
 				throw new RuntimeException(e);
