5f05459bcbf374e69d856d45f231d1b4d01dd343
Yuku on yuku9 <yukuku@gmail.com>
2019-10-16 17:05:01 +0800

    Integrate AlkitabIntegrationTest to AlkitabIntegration

--- AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/LauncherTest.java
+++ AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/LauncherTest.java
@@ -1,13 +1,9 @@
 package yuku.alkitabintegration.test;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.test.AndroidTestCase;
 
 import yuku.alkitabintegration.display.Launcher;
-import yuku.alkitabintegration.display.Launcher.Product;
-
-import static junit.framework.Assert.*;
 
 
 public class LauncherTest extends AndroidTestCase {
@@ -17,18 +13,11 @@ public class LauncherTest extends AndroidTestCase {
 			assertEquals("yuku.alkitab.action.VIEW", intent.getAction());
 			assertEquals(0x220102, intent.getIntExtra("ari", -1));
 		}
-		
+
 		{
 			Intent intent = Launcher.openAppAtBibleLocation(0x121110);
 			assertEquals("yuku.alkitab.action.VIEW", intent.getAction());
 			assertEquals(0x121110, intent.getIntExtra("ari", -1));
-		}
-		
-		{
-			Intent intent = Launcher.openGooglePlayDownloadPage(getContext(), Product.ALKITAB);
-			assertEquals("market", intent.getData().getScheme());
-			assertEquals(Product.ALKITAB.getPackageName(), intent.getData().getQueryParameter("id"));
-			assertEquals(getContext().getPackageName(), Uri.parse("whatever://whatever?" + intent.getData().getQueryParameter("referrer")).getQueryParameter("utm_medium"));
 		}
 	}
 }
--- AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/VerseProviderTest.java
+++ AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/VerseProviderTest.java
@@ -2,38 +2,40 @@ package yuku.alkitabintegration.test;
 
 import android.content.ContentResolver;
 import android.test.AndroidTestCase;
+
+import java.util.List;
+
 import yuku.alkitabintegration.AlkitabIntegrationUtil;
 import yuku.alkitabintegration.ConnectionResult;
 import yuku.alkitabintegration.provider.VerseProvider;
 import yuku.alkitabintegration.provider.VerseProvider.Verse;
 import yuku.alkitabintegration.provider.VerseProvider.VerseRanges;
 
-import java.util.List;
-
 public class VerseProviderTest extends AndroidTestCase {
 
 	@Override
 	public void setUp() throws Exception {
+		super.setUp();
 		AlkitabIntegrationUtil.setOverridenProviderAuthority("yuku.alkitab.debug.provider");
 	}
 
-	public void testAvailable() throws Exception {
+	public void testAvailable() {
 		assertEquals(ConnectionResult.SUCCESS, AlkitabIntegrationUtil.isIntegrationAvailable(getContext()));
 	}
 
 	public void testSingleVerse() throws Throwable {
 		ContentResolver cr = getContext().getContentResolver();
 		VerseProvider vp = new VerseProvider(cr);
-		
+
 		int ari = 0x000103;
 		Verse v = vp.getVerse(0, 1, 3);
 		assertNotNull(v);
-		
+
 		assertEquals(v.ari, ari);
 		assertNotNull(v.bookName);
 		assertNotNull(v.text);
 		assertTrue(v.text.length() > 0);
-		
+
 		assertNotNull(v.toString());
 		assertEquals(v.toString().substring(0, v.bookName.length()), v.bookName);
 
@@ -43,25 +45,25 @@ public class VerseProviderTest extends AndroidTestCase {
 		// Gen 1:32 should be null
 		assertNull(vp.getVerse(0x000120));
 	}
-	
+
 	public void testVerseRanges() throws Throwable {
 		ContentResolver cr = getContext().getContentResolver();
 		VerseProvider vp = new VerseProvider(cr);
-		
+
 		VerseRanges ranges = new VerseRanges();
-		
+
 		{
 			ranges.addRange(0, 1, 1, 0, 1, 1);
 			List<Verse> verses = vp.getVerses(ranges);
 			assertEquals(1, verses.size());
 		}
-		
+
 		{
 			ranges.addRange(0, 1, 1, 0, 1, 10);
 			List<Verse> verses = vp.getVerses(ranges);
 			assertEquals(11, verses.size());
 		}
-		
+
 		{
 			ranges.addRange(1, 2, 25, 1, 3, 1);
 			List<Verse> verses = vp.getVerses(ranges);
@@ -69,7 +71,7 @@ public class VerseProviderTest extends AndroidTestCase {
 			assertEquals(0x010301, verses.get(12).ari);
 			assertFalse(verses.get(0).bookName.equals(verses.get(11).bookName));
 		}
-		
+
 		{ // should not have formatting codes
 			ranges.addRange(2, 1, 1, 3, 1, 1);
 			ranges.addRange(4, 1, 1, 4, 1, 1);
