d88c4fd40b4ae299f32e50847b2c0ac2811e0866
Yuku on yuku9 <yukuku@gmail.com>
2019-10-17 20:02:04 +0800

    AndroidX migration: Firebase core 17.2.0, messaging 20.0.0

--- Alkitab/src/main/java/yuku/alkitab/base/sync/FcmMessagingService.kt
+++ Alkitab/src/main/java/yuku/alkitab/base/sync/FcmMessagingService.kt
@@ -9,22 +9,14 @@ private const val TAG = "FcmMessagingService"
 
 class FcmMessagingService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        remoteMessage ?: return
-
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         AppLog.d(TAG, "@@onReceive from: ${remoteMessage.from}")
-
-        val data = remoteMessage.data
-        if (data == null) {
-            AppLog.e(TAG, "@@onReceive remote message has no data")
-            return
-        }
 
         val intent = Intent(this, FcmIntentService::class.java)
 
         // convert data map to intent extras
-        data.forEach { (k, v) ->
+        remoteMessage.data.forEach { (k, v) ->
             if (v != null) {
                 intent.putExtra(k, v)
             }
@@ -33,9 +25,8 @@ class FcmMessagingService : FirebaseMessagingService() {
         FcmIntentService.enqueueWork(this, intent)
     }
 
-    override fun onNewToken(token: String?) {
+    override fun onNewToken(token: String) {
         AppLog.d(TAG, "Refreshed token: $token")
-        token ?: return
 
         Sync.notifyNewFcmRegistrationId(token)
     }
