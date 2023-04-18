3361b4f35de08bd304c24f0fbeaa71c04e663696
Yuku on yuku9 <yukuku@gmail.com>
2023-04-18 17:17:51 +0800

    Fix AlkitabIntegration tests

--- AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/LauncherTest.java
+++ AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/LauncherTest.java
@@ -2,28 +2,25 @@ package yuku.alkitabintegration.test;
 
 import android.content.Intent;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import yuku.alkitabintegration.display.Launcher;
-
-import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
 public class LauncherTest {
-	@Test
-	public void testOpenApp() {
-		{
-			Intent intent = Launcher.openAppAtBibleLocation(0x22, 0x01, 0x02);
-			assertEquals("yuku.alkitab.action.VIEW", intent.getAction());
-			assertEquals(0x220102, intent.getIntExtra("ari", -1));
-		}
+    @Test
+    public void testOpenApp() {
+        {
+            Intent intent = Launcher.openAppAtBibleLocation(0x22, 0x01, 0x02);
+            assertEquals("yuku.alkitab.action.VIEW", intent.getAction());
+            assertEquals(0x220102, intent.getIntExtra("ari", -1));
+        }
 
-		{
-			Intent intent = Launcher.openAppAtBibleLocation(0x121110);
-			assertEquals("yuku.alkitab.action.VIEW", intent.getAction());
-			assertEquals(0x121110, intent.getIntExtra("ari", -1));
-		}
-	}
+        {
+            Intent intent = Launcher.openAppAtBibleLocation(0x121110);
+            assertEquals("yuku.alkitab.action.VIEW", intent.getAction());
+            assertEquals(0x121110, intent.getIntExtra("ari", -1));
+        }
+    }
 }
--- AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/VerseProviderTest.java
+++ AlkitabIntegration/src/androidTest/java/yuku/alkitabintegration/test/VerseProviderTest.java
@@ -2,104 +2,100 @@ package yuku.alkitabintegration.test;
 
 import android.content.ContentResolver;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
+import androidx.test.platform.app.InstrumentationRegistry;
 import java.util.List;
-
-import yuku.alkitabintegration.AlkitabIntegrationUtil;
-import yuku.alkitabintegration.ConnectionResult;
-import yuku.alkitabintegration.provider.VerseProvider;
-import yuku.alkitabintegration.provider.VerseProvider.Verse;
-import yuku.alkitabintegration.provider.VerseProvider.VerseRanges;
-
-import static androidx.test.platform.app.InstrumentationRegistry.getContext;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import yuku.alkitabintegration.AlkitabIntegrationUtil;
+import yuku.alkitabintegration.ConnectionResult;
+import yuku.alkitabintegration.provider.VerseProvider;
+import yuku.alkitabintegration.provider.VerseProvider.Verse;
+import yuku.alkitabintegration.provider.VerseProvider.VerseRanges;
 
 @RunWith(AndroidJUnit4.class)
 public class VerseProviderTest {
 
-	@Before
-	public void setUp() {
-		AlkitabIntegrationUtil.setOverridenProviderAuthority("yuku.alkitab.debug.provider");
-	}
+    @Before
+    public void setUp() {
+        AlkitabIntegrationUtil.setOverridenProviderAuthority("yuku.alkitab.debug.provider");
+    }
 
-	@Test
-	public void testAvailable() {
-		assertEquals(ConnectionResult.SUCCESS, AlkitabIntegrationUtil.isIntegrationAvailable(getContext()));
-	}
+    @Test
+    public void testAvailable() {
+        assertEquals(ConnectionResult.SUCCESS, AlkitabIntegrationUtil.isIntegrationAvailable(InstrumentationRegistry.getInstrumentation().getTargetContext()));
+    }
 
-	@Test
-	public void testSingleVerse() {
-		ContentResolver cr = getContext().getContentResolver();
-		VerseProvider vp = new VerseProvider(cr);
+    @Test
+    public void testSingleVerse() {
+        ContentResolver cr = InstrumentationRegistry.getInstrumentation().getTargetContext().getContentResolver();
+        VerseProvider vp = new VerseProvider(cr);
 
-		int ari = 0x000103;
-		Verse v = vp.getVerse(0, 1, 3);
-		assertNotNull(v);
+        int ari = 0x000103;
+        Verse v = vp.getVerse(0, 1, 3);
+        assertNotNull(v);
 
-		assertEquals(v.ari, ari);
-		assertNotNull(v.bookName);
-		assertNotNull(v.text);
-		assertTrue(v.text.length() > 0);
+        assertEquals(v.ari, ari);
+        assertNotNull(v.bookName);
+        assertNotNull(v.text);
+        assertTrue(v.text.length() > 0);
 
-		assertNotNull(v.toString());
-		assertEquals(v.toString().substring(0, v.bookName.length()), v.bookName);
+        assertNotNull(v.toString());
+        assertEquals(v.toString().substring(0, v.bookName.length()), v.bookName);
 
-		// Gen 1:31 should have text
-		assertNotNull(vp.getVerse(0x00011f));
+        // Gen 1:31 should have text
+        assertNotNull(vp.getVerse(0x00011f));
 
-		// Gen 1:32 should be null
-		assertNull(vp.getVerse(0x000120));
-	}
+        // Gen 1:32 should be null
+        assertNull(vp.getVerse(0x000120));
+    }
 
-	@Test
-	public void testVerseRanges() throws Throwable {
-		ContentResolver cr = getContext().getContentResolver();
-		VerseProvider vp = new VerseProvider(cr);
+    @Test
+    public void testVerseRanges() {
+        ContentResolver cr = InstrumentationRegistry.getInstrumentation().getTargetContext().getContentResolver();
+        VerseProvider vp = new VerseProvider(cr);
 
-		VerseRanges ranges = new VerseRanges();
+        VerseRanges ranges = new VerseRanges();
 
-		{
-			ranges.addRange(0, 1, 1, 0, 1, 1);
-			List<Verse> verses = vp.getVerses(ranges);
-			assertEquals(1, verses.size());
-		}
+        {
+            ranges.addRange(0, 1, 1, 0, 1, 1);
+            List<Verse> verses = vp.getVerses(ranges);
+            assertEquals(1, verses.size());
+        }
 
-		{
-			ranges.addRange(0, 1, 1, 0, 1, 10);
-			List<Verse> verses = vp.getVerses(ranges);
-			assertEquals(11, verses.size());
-		}
+        {
+            ranges.addRange(0, 1, 1, 0, 1, 10);
+            List<Verse> verses = vp.getVerses(ranges);
+            assertEquals(11, verses.size());
+        }
 
-		{
-			ranges.addRange(1, 2, 25, 1, 3, 1);
-			List<Verse> verses = vp.getVerses(ranges);
-			assertEquals(13, verses.size());
-			assertEquals(0x010301, verses.get(12).ari);
-			assertNotEquals(verses.get(0).bookName, verses.get(11).bookName);
-		}
+        {
+            ranges.addRange(1, 2, 25, 1, 3, 1);
+            List<Verse> verses = vp.getVerses(ranges);
+            assertEquals(13, verses.size());
+            assertEquals(0x010301, verses.get(12).ari);
+            assertNotEquals(verses.get(0).bookName, verses.get(11).bookName);
+        }
 
-		{ // should not have formatting codes
-			ranges.addRange(2, 1, 1, 3, 1, 1);
-			ranges.addRange(4, 1, 1, 4, 1, 1);
-			ranges.addRange(60, 1, 1, 60, 1, 1);
-			ranges.addRange(61, 1, 1, 61, 1, 1);
-			ranges.addRange(62, 1, 1, 62, 1, 1);
-			ranges.addRange(63, 1, 1, 63, 1, 1);
-			ranges.addRange(64, 1, 1, 64, 1, 1);
-			ranges.addRange(65, 1, 1, 65, 1, 1);
-			List<Verse> verses = vp.getVerses(ranges);
-			for (Verse verse : verses) {
-				assertFalse(verse.text.contains("@"));
-			}
-		}
-	}
+        { // should not have formatting codes
+            ranges.addRange(2, 1, 1, 3, 1, 1);
+            ranges.addRange(4, 1, 1, 4, 1, 1);
+            ranges.addRange(60, 1, 1, 60, 1, 1);
+            ranges.addRange(61, 1, 1, 61, 1, 1);
+            ranges.addRange(62, 1, 1, 62, 1, 1);
+            ranges.addRange(63, 1, 1, 63, 1, 1);
+            ranges.addRange(64, 1, 1, 64, 1, 1);
+            ranges.addRange(65, 1, 1, 65, 1, 1);
+            List<Verse> verses = vp.getVerses(ranges);
+            for (Verse verse : verses) {
+                assertFalse(verse.text.contains("@"));
+            }
+        }
+    }
 }
