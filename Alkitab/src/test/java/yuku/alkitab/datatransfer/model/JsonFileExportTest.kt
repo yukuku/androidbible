121ea9141d7247a553b45fef198a05b94503ed6e
Yuku on yuku12 <yukuku@gmail.com>
2023-04-28 12:16:07 +0800

    Fix unit test

--- Alkitab/src/test/java/yuku/alkitab/datatransfer/model/JsonFileExportTest.kt
+++ Alkitab/src/test/java/yuku/alkitab/datatransfer/model/JsonFileExportTest.kt
@@ -1,15 +1,20 @@
 package yuku.alkitab.datatransfer.model
 
-import java.io.File
 import kotlinx.serialization.decodeFromString
+import org.junit.Assert.assertNotNull
 import org.junit.Test
 import yuku.alkitab.datatransfer.process.Serializer
 
 class JsonFileExportTest {
     @Test
     fun testParse() {
-        val json = File("/tmp/download_all.json").readText()
-        val root = Serializer.importJson.decodeFromString<Root>(json)
-        println(root)
+        val resourceInput = this::class.java.getResourceAsStream("/fixtures/download_all.json")
+        assertNotNull(resourceInput)
+
+        if (resourceInput != null) {
+            val json = resourceInput.reader().use { it.readText() }
+            val root = Serializer.importJson.decodeFromString(Root.serializer(), json)
+            println(root)
+        }
     }
 }
