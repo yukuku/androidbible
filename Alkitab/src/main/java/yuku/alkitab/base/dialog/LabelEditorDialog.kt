4114b7b70efb64ee085f57b148d7738d9ce3d49f
Yuku on yuku12 <yukuku@gmail.com>
2023-05-02 14:18:04 +0800

    Replace label edit dialog layout with material dialog input

--- Alkitab/src/main/java/yuku/alkitab/base/dialog/LabelEditorDialog.kt
+++ Alkitab/src/main/java/yuku/alkitab/base/dialog/LabelEditorDialog.kt
@@ -1,65 +1,57 @@
 package yuku.alkitab.base.dialog
 
 import android.content.Context
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
-import android.widget.EditText
+import android.text.InputType
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.WhichButton
 import com.afollestad.materialdialogs.actions.setActionButtonEnabled
-import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.input.input
 import yuku.alkitab.base.S.db
 import yuku.alkitab.debug.R
+
+private const val MAX_LABEL_LENGTH = 48
 
 object LabelEditorDialog {
     @JvmStatic
     fun show(context: Context, initialText: String, title: String, okListener: OkListener) {
-        val dialogView = View.inflate(context, R.layout.dialog_edit_label, null)
-
-        val tCaption = dialogView.findViewById<EditText>(R.id.tCaption)
-        tCaption.setText(initialText)
+        val allLabels = db.listAllLabels()
+        var input = ""
 
         val dialog = MaterialDialog(context)
-            .customView(view = dialogView)
             .title(text = title)
+            .input(
+                hintRes = R.string.nama_label_titikdua,
+                prefill = initialText,
+                inputType = InputType.TYPE_CLASS_TEXT,
+                maxLength = MAX_LABEL_LENGTH,
+                allowEmpty = false,
+                waitForPositiveButton = false,
+            ) { dialog, s ->
+                input = s.toString().trim()
+
+                if (s.isBlank() || s.length > MAX_LABEL_LENGTH) {
+                    dialog.setActionButtonEnabled(WhichButton.POSITIVE, false)
+                    return@input
+                } else {
+                    val newTitleTrimmed = s.toString().trim()
+                    for (label in allLabels) {
+                        if (label.title.trim() == newTitleTrimmed) {
+                            dialog.setActionButtonEnabled(WhichButton.POSITIVE, false)
+                            return@input
+                        }
+                    }
+                }
+
+                dialog.setActionButtonEnabled(WhichButton.POSITIVE, true)
+            }
             .positiveButton(R.string.ok) {
-                okListener.onOk(tCaption.text.toString().trim())
+                okListener.onOk(input)
             }
             .negativeButton(R.string.cancel)
-
-        val window = dialog.window
-        if (window != null) {
-            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-        }
 
         dialog.show()
 
         dialog.setActionButtonEnabled(WhichButton.POSITIVE, false)
-
-        val allLabels = db.listAllLabels()
-        tCaption.addTextChangedListener(object : TextWatcher {
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-            override fun afterTextChanged(s: Editable) {
-                if (s.isBlank()) {
-                    dialog.setActionButtonEnabled(WhichButton.POSITIVE, false)
-                    return
-                } else {
-                    val newTitleTrimmed = s.toString().trim { it <= ' ' }
-                    for (label in allLabels) {
-                        if (label.title.trim() == newTitleTrimmed) {
-                            dialog.setActionButtonEnabled(WhichButton.POSITIVE, false)
-                            return
-                        }
-                    }
-                }
-                dialog.setActionButtonEnabled(WhichButton.POSITIVE, true)
-            }
-        })
     }
 
     fun interface OkListener {
