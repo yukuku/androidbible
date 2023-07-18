c621e3737030ba743e5c434d3556df8612af6e61
Yuku on yuku12 <yukuku@gmail.com>
2023-07-18 14:28:41 +0800

    Use atomic boolean to record app initialization, again

--- Afw/src/main/java/yuku/afw/App.java
+++ Afw/src/main/java/yuku/afw/App.java
@@ -1,11 +1,14 @@
 package yuku.afw;
 
+import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class App extends Application {
-    private static boolean initted = false;
+    private static final AtomicBoolean initted = new AtomicBoolean(false);
 
+    @SuppressLint("StaticFieldLeak") // This is application context, safe to keep references to
     public static Context context;
 
     @Override
@@ -16,13 +19,12 @@ public class App extends Application {
     }
 
     public static void initWithAppContext(Context applicationContext) {
-        if (initted) return;
+        if (initted.getAndSet(true)) return;
 
         if (applicationContext == null) {
             throw new RuntimeException("Application context can't be null");
         }
 
-        initted = true;
         context = applicationContext;
     }
 }
