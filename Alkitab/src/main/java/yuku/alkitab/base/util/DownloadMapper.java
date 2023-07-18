84877d839f19903736a05890a9cfbfcab34d8276
Yuku on yuku12 <yukuku@gmail.com>
2023-07-18 14:34:26 +0800

    Remove notifications on download mapper

--- Alkitab/src/main/java/yuku/alkitab/base/util/DownloadMapper.java
+++ Alkitab/src/main/java/yuku/alkitab/base/util/DownloadMapper.java
@@ -1,24 +1,16 @@
 package yuku.alkitab.base.util;
 
 import android.app.DownloadManager;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
-import android.text.format.Formatter;
 import android.util.Log;
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import com.downloader.Error;
 import com.downloader.OnDownloadListener;
 import com.downloader.PRDownloader;
 import com.downloader.Status;
 import com.downloader.request.DownloadRequest;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import yuku.alkitab.base.App;
@@ -31,8 +23,6 @@ public enum DownloadMapper {
     instance;
 
     static final String TAG = DownloadMapper.class.getSimpleName();
-    static final String NOTIF_TAG = "DownloadMapper";
-    static final String NOTIFICATION_CHANNEL_ID = "download_mapper";
 
     static class Row {
         public int id;
@@ -45,7 +35,7 @@ public enum DownloadMapper {
          * When is the last time the progress is notified (in currentTimeMillis).
          * We should not notify about progress too often.
          */
-        public long previouslyNotifiedTime;
+        public long previouslyUpdatedProgressTime;
         public Map<String, String> attrs;
     }
 
@@ -64,11 +54,6 @@ public enum DownloadMapper {
 
     public float getDownloadProgress(final String downloadKey) {
         final Row row = currentByKey.get(downloadKey);
-        return getDownloadProgress(row);
-    }
-
-    public float getDownloadProgress(final int id) {
-        final Row row = currentById.get(id);
         return getDownloadProgress(row);
     }
 
@@ -158,12 +143,10 @@ public enum DownloadMapper {
             row.currentBytes = progress.currentBytes;
             row.totalBytes = progress.totalBytes;
 
-            // only notify if last notification is not recent enough.
+            // only notify progress if last progress update is not recent enough.
             final long now = System.currentTimeMillis();
-            if (row.previouslyNotifiedTime == 0 || now - row.previouslyNotifiedTime > 1000) {
-                row.previouslyNotifiedTime = now;
-
-                displayNotifs();
+            if (row.previouslyUpdatedProgressTime == 0 || now - row.previouslyUpdatedProgressTime > 250) {
+                row.previouslyUpdatedProgressTime = now;
                 App.getLbm().sendBroadcast(new Intent(VersionListFragment.ACTION_RELOAD));
             }
         });
@@ -208,39 +191,13 @@ public enum DownloadMapper {
         row.attrs = new LinkedHashMap<>(attrs);
         currentByKey.put(downloadKey, row);
         currentById.put(row.id, row);
-        displayNotifs();
-    }
-
-    void displayNotifs() {
-        final Context context = App.context;
-
-        final NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, context.getString(R.string.notification_channel_download_mapper_name), NotificationManager.IMPORTANCE_LOW);
-        final NotificationManager nm = App.context.getSystemService(NotificationManager.class);
-        if (nm != null) nm.createNotificationChannel(channel);
-
-        final NotificationManagerCompat nmc = NotificationManagerCompat.from(context);
-        for (final Row row : new ArrayList<>(currentById.values())) {
-            final Notification n = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(android.R.drawable.stat_sys_download)
-                .setContentTitle(row.title)
-                .setContentText(row.totalBytes <= 0 ? "" : (Formatter.formatFileSize(context, row.currentBytes) + " / " + Formatter.formatFileSize(context, row.totalBytes)))
-                .setShowWhen(false)
-                .setProgress(1000, row.totalBytes == 0 ? 0 : (int) (1000 * row.currentBytes / row.totalBytes), false)
-                .setOngoing(true)
-                .build();
-
-            nmc.notify(NOTIF_TAG, row.id, n);
-        }
     }
 
     /**
-     * Stop and remove from in-memory list and notification.
+     * Stop and remove from in-memory list.
      */
     public void remove(final int id) {
         PRDownloader.cancel(id);
-
-        final NotificationManagerCompat nmc = NotificationManagerCompat.from(App.context);
-        nmc.cancel(NOTIF_TAG, id);
 
         final Row row = currentById.get(id);
         if (row != null) {
